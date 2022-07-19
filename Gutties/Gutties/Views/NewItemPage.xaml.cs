73147aa75071460ecb4006203b341674f2d1d233
Griiffiin <49454046+Griiffiin@users.noreply.github.com>
2022-07-19 11:36:24 -0400

    Add project files.

---
+++ Gutties/Gutties/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Gutties.Models;
+using Gutties.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gutties.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
